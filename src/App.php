a6b8b456a937ed2854f509a04acb452525bd0f79
Thomas Elvin <thom855j@cvkweb.dk>
2015-10-03 17:50:13 +0200

    better config set
    can  access from “.” as array

--- src/App.php
+++ src/App.php
@@ -15,7 +15,7 @@ class App extends Router
 	private
 		$_data;
 
-	public function __construct($params)
+	public function bootstrap($params)
 	{
 		if (!empty($params)) {
 			foreach ($params as $key => $value) {
@@ -42,14 +42,36 @@ class App extends Router
 
 	public function set($case, $input)
 	{
-		static $data = array();
-		array_push($data, $input);
-		return $this->_data[$case] = $data;
+		return $this->_data[$case] = $input;
 	}
 
-	public function get($case)
+	public function get($index, $default = null)
 	{
-		return $this->_data[$case];
+		$index = explode('.', $index);
+		$data = $this->_getValue($index, $this->_data);
+		if ($data) {
+			return $data;
+		}
+		return FALSE;
+	}
+
+	private function _getValue($index, $value)
+	{
+		if (is_array($index) and
+			count($index)) {
+			$current_index = array_shift($index);
+		}
+		if (is_array($index) and
+			count($index) and
+			isset($value[$current_index]) and
+			is_array($value[$current_index]) and
+			count($value[$current_index])) {
+			return $this->_getValue($index, $value[$current_index]);
+		} elseif (isset($value[$current_index])) {
+			return $value[$current_index];
+		} else {
+			return FALSE;
+		}
 	}
 
 	public function run()
