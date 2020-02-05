e590a6f2e132de79d8b2cecb82453ead81d12f6a
Thomas Elvin <info@datalaere.dk>
2020-02-05 14:26:15 +0100

    fixed
    missing namespace

--- src/Controller.php
+++ src/Controller.php
@@ -6,7 +6,7 @@
  * 2. check if the user is not logged in anymore (session timeout) but has a cookie
  */
 
-namespace Datalaere\PHPMvcFramework;
+namespace PHP\MVC;
 
 abstract class Controller
 {
@@ -26,5 +26,74 @@ abstract class Controller
 
         // create a app object to be able to use it inside a controller, like $this->App
         $this->container = $container;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     */
+    public function store()
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     */
+    public function show($id)
+    {
+        return view('notifications.show', [
+            'notifications' => tap(auth()->user()->unreadNotifications)->markAsRead()
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  int  $id
+     */
+    public function update($id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     */
+    public function destroy($id)
+    {
+        //
     }
 }
